a2e71b70940571464e2fb792932e8111251ff285
2100080051 <129878824+2100080051@users.noreply.github.com>
2025-08-22 09:21:50 +0530

    Update text_processing.py

--- utils/text_processing.py
+++ utils/text_processing.py
@@ -1,12 +1,19 @@
-import vertexai
-from vertexai.generative_models import GenerativeModel
+import streamlit as st
+import os, json
 
-PROJECT_ID = "my-project-8-465221"
-LOCATION = "us-central1"
+# Save service account JSON from secrets to file
+with open("gcp_key.json", "w") as f:
+    f.write(json.dumps(st.secrets["google_service_account"]))
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "gcp_key.json"
+
+PROJECT_ID = st.secrets["PROJECT_ID"]
+LOCATION = st.secrets["LOCATION"]
 
 vertexai.init(project=PROJECT_ID, location=LOCATION)
 
 model = GenerativeModel("gemini-2.0-flash")
+
 
 def generate_story(concept: str, language: str = "English", max_words=500):
     """Generate a story based on a concept and language"""
@@ -18,3 +25,4 @@ def split_into_chunks(text, max_words=60):
     """Split story into smaller chunks (pages)."""
     words = text.split()
     return [" ".join(words[i:i+max_words]) for i in range(0, len(words), max_words)]
+
